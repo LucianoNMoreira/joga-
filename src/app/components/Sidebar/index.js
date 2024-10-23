1f0d0567a45936d88dc80d05801ff4be021ee17c
Luciano Nobre Moreira <lucianonobremoreira@gmail.com>
2024-10-23 11:57:10 -0300

    ação de logout

--- src/app/components/Sidebar/index.js
+++ src/app/components/Sidebar/index.js
@@ -1,15 +1,28 @@
+'use client'
+
+import Link from 'next/link'
 import './styles.css'
+import { redirect } from "next/navigation"
+import Cookies from 'js-cookie'
 
 export default function Sidebar() {
+    const handleLogout = () => {
+        Cookies.set('user', null)
+        window.location.href = '/'
+    }
+
     return(
         <div id='sidebar-component'>
             <h3>Sidebar</h3>
             <ul>
                 <li>
-                    <a href='/'>Início</a>
+                    <Link href='/'>Início</Link>
                 </li>
                 <li>
                     <a href='/login'>Login</a>
+                </li>
+                <li>
+                    <button onClick={ handleLogout }>Sair</button>
                 </li>
             </ul>
         </div>
