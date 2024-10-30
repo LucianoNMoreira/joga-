2bd8ab1e66bf128b1be0b8b24f44cf6ea273dda7
Luciano Nobre Moreira <lucianonobremoreira@gmail.com>
2024-10-30 12:34:14 -0300

    teste do sidebar com usuário logado

--- src/app/components/Sidebar/index.test.js
+++ src/app/components/Sidebar/index.test.js
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom'
 import { render, screen } from "@testing-library/react"
 import Sidebar, { createMenuItems } from "."
 import { UserProvider } from "@/app/Contexts/UserContext"
+import Cookies from 'js-cookie'
 
 describe('Testes da função Sidebar.createMenuItems', () => {
 
@@ -30,6 +31,23 @@ describe('Testes de renderização do Sidebar', () => {
     )
 
     expect(screen.queryByTestId('logout-button')).not.toBeInTheDocument()
+  })
+
+  test('Para usuários logados deve exibir o link #logout', () => {
+    const user = {
+      id: 1,
+      name: 'Luciano',
+      email: 'luciano@email.com'
+    }
+    Cookies.set('user', JSON.stringify(user))
+
+    render(
+      <UserProvider>
+        <Sidebar />
+      </UserProvider>
+    )
+
+    expect(screen.queryByTestId('logout-button')).toBeInTheDocument()
   })
 
 })
