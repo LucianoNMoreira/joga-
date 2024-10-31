327cd34e6932c32ac0e9c4d0cc390bedcef208ef
Luciano Nobre Moreira <lucianonobremoreira@gmail.com>
2024-10-31 15:04:02 -0300

    SEO: sitemap.xml

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'http://localhost:3000/esportes',
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 1
+        }
+    ]
+}
