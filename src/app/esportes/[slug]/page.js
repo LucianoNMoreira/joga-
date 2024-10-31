e4ff0db5d2f147194546ee6832d01dc7ad10ad0e
Luciano Nobre Moreira <lucianonobremoreira@gmail.com>
2024-10-31 15:04:10 -0300

    SEO: Metatags

--- src/app/esportes/[slug]/page.js
+++ src/app/esportes/[slug]/page.js
@@ -1,3 +1,13 @@
+export async function generateMetadata({ params, searchParams }, parent) {
+  const { slug } = await params
+  
+  // Resgatar o conteúdo
+
+  return {
+    title: slug,
+    description: `Página sobre ${slug}`
+  }
+}
 
 export default async function Page({ params }) {
   const { slug } = await params
